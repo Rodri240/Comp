d5c0c82fe140227f1b1438eda009b9623769cff4
Rodri240 <[rodrigo.pereira@correo.ucu.edu.uy]>
2024-05-27 12:06:50 -0300

    Falta Ej4

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -13,10 +13,20 @@ namespace CompAndDel
             PipeNull pipeNull = new PipeNull();
             FilterNegative negative = new FilterNegative();
             PipeSerial serial = new PipeSerial (negative, pipeNull);
+            CognitiveFace cog = new CognitiveFace(true, Color.GreenYellow);
+            PipeFork pipeFork = new PipeFork()
+
+           
             FilterGreyscale greyscale = new FilterGreyscale();
             PipeSerial serialtwo = new PipeSerial(greyscale, serial);
             provider.SavePicture(serialtwo.Send(picture), @"C:\Users\uru24\OneDrive\Pictures\newLuke.jpg");
-            provider.SavePicture(serial.Send(picture), @"C:\Users\uru24\OneDrive\Pictures\newLuke2.jpg");
+
+            // Paso previo al flitro intermedio
+           // provider.SavePicture(serial.Send(picture), @"C:\Users\uru24\OneDrive\Pictures\newLuke2.jpg");
+
+            //Publicar en Twitter
+            var twitter = new TwitterImage();
+            Console.WriteLine(twitter.PublishToTwitter("Luke", @"C:\Users\uru24\OneDrive\Pictures\newLuke.jpg"));
             
 
         }
