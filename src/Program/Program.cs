dca14ad878803b85005392651b6b954eb6447d37
Rodri240 <163062189+Rodri240@users.noreply.github.com>
2024-05-27 11:30:47 -0300

    Ejercicio 2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,17 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"C:\Users\uru24\Prog2\Comp\src\Program\luke.jpg");
+            PipeNull pipeNull = new PipeNull();
+            FilterNegative negative = new FilterNegative();
+            PipeSerial serial = new PipeSerial (negative, pipeNull);
+            FilterGreyscale greyscale = new FilterGreyscale();
+            PipeSerial serialtwo = new PipeSerial(greyscale, serial);
+            provider.SavePicture(serialtwo.Send(picture), @"C:\Users\uru24\OneDrive\Pictures\newLuke.jpg");
+            provider.SavePicture(serial.Send(picture), @"C:\Users\uru24\OneDrive\Pictures\newLuke2.jpg");
+            
+
         }
     }
 }
